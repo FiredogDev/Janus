78f8901c6ecd3e15e73e99055d26b32db6aeed70
ammar88 <ammar.idris@aimail.info>
2015-03-18 17:44:20 +0000

    refactored 'hentry by layout' template files

--- partials/hentry/meta/meta-category.php
+++ partials/hentry/meta/meta-category.php
@@ -1,1 +1,7 @@
-<span class="hentry__meta__category"><?php echo get_the_category_list(', '); ?></span>
+<?php
+if ($post->type = 'post') { $meta__post_type = "Our Thinking: "; }
+else{ $meta__post_type = "Case Study: "; } ?>
+<span class="hentry__meta__category">
+	<span class="hentry__meta__post_type"><?php echo $meta__post_type; ?></span>
+	<?php echo get_the_category_list(', '); ?>
+</span>
