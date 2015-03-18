e8f9e46401fe3b300ab5931ba65c054860f5a6c5
ammar88 <ammar.idris@aimail.info>
2015-03-18 11:14:44 +0000

    Added new typeface, amended hero unit on featured article page

--- single.php
+++ single.php
@@ -10,12 +10,12 @@
 					<div class="hero_unit__wrapper width--full cf">
 						<div class="hero_unit__header cf">
 							<!-- Categoriszation -->
-							<div class="hero_unit__category_byline width--full cf text-center">
+							<div class="hero_unit__category_byline width--full cf">
 								<?php get_template_part( 'partials/hentry/part', 'byline' ); ?>
 							</div>
 							<h1 class="hero_unit__title text-center width--full" itemprop="headline">
 								<!-- Main title -->
-								<span class="hero_unit__title--main"><?php the_title(); ?></span>
+								<span class="hero_unit__title--main width--full fl"><?php the_title(); ?></span>
 							</h1>
 						</div>
 						<div class="hero_unit__copy text-center width--full">
