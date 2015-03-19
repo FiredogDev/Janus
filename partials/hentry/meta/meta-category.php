686ec7409725bad7b5cf91a0e91a75a1adf6d728
ammar88 <ammar.idris@aimail.info>
2015-03-19 17:49:28 +0000

    init on refactor hentry meta

--- partials/hentry/meta/meta-category.php
+++ partials/hentry/meta/meta-category.php
@@ -1,7 +1,10 @@
 <?php
-if ($post->type = 'post') { $meta__post_type = "Our Thinking: "; }
-else{ $meta__post_type = "Case Study: "; } ?>
+if ($post->type = 'post') { 
+	$meta__post_title_prefix = "Our Thinking: ";
+}else{ 
+	$meta__post_title_prefix = "Case Study: ";
+} ?>
 <span class="hentry__meta__category">
-	<span class="hentry__meta__post_type"><?php echo $meta__post_type; ?></span>
+	<span class="hentry__meta__entry_prefix"><?php echo $meta__post_type; ?></span>
 	<?php echo get_the_category_list(', '); ?>
 </span>
--- partials/hentry/meta/meta-comment_count.php
+++ partials/hentry/meta/meta-comment_count.php
@@ -1,5 +1,4 @@
 <?php 
-// 
 $comments_form_url = get_the_permalink() . '/#comments';
 $leave_a_comment_link = '<a href="' . $comments_form_url . '">Leave a comment</a>'; ?>
 <span class="hentry__meta__comment_count">
