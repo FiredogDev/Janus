9f0b5ee0751c7eb09d679fa74f93e8d9d61a48f3
ammar88 <ammar.idris@aimail.info>
2015-04-02 09:19:50 +0100

    Added featured image to showcase entry

--- partials/hentry/post/as-showcase.php
+++ partials/hentry/post/as-showcase.php
@@ -1,5 +1,18 @@
+<?php if (has_post_thumbnail()){
+	// Thumb ID
+	$post_thumb_id 			= get_post_thumbnail_id($post->ID);
+	// Sizes...
+	$featured_image_large 	= wp_get_attachment_image_src( $post_thumb_id, 'large' );
+} //end if ?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class( "showcase__entry hoverboard__entry full_height prel fl" ); ?> role="article">
-	<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" class="pabs pcover showcase__entry__permalink"></a>
+	<a 	href="<?php the_permalink(); ?>"
+		rel="bookmark" 
+		title="<?php the_title_attribute(); ?>" 
+		class="pabs pcover showcase__entry__featured_image"
+		style="background-image: url(<?php echo $featured_image_large['0']; //Large ?>);" >
+	</a>
+
 	<header class="hentry__header showcase__entry__header pabs">
 		<p class="hentry__meta">
 			<?php // CATEGORY & BYLINE
@@ -8,5 +21,4 @@
 		</p>
 		<h3 class="hentry__title showcase__title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 	</header>
-	
 </article>
