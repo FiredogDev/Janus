4378419a1276bb2e9547e0a67d9e4a21f02834f2
ammar88 <ammar.idris@aimail.info>
2015-04-08 12:38:51 +0100

    added unbind on button during request

--- library/js/app/viewmore.js
+++ library/js/app/viewmore.js
@@ -18,7 +18,7 @@ define([
 			t.button = button;
 			t.button_fill = t.button.find('.viewmore__fill');
 
-			t.setup_event_listeners();
+			t.setup_button_event_handlers();
 	}
 
 	Viewmore.prototype.button = null;
@@ -26,76 +26,64 @@ define([
 	Viewmore.prototype.incomming_posts = null;
 	Viewmore.prototype.current_ready_state = 0;
 
-	Viewmore.prototype.setup_event_listeners = function(){
-		var t = this,
-			_c_s = _c.SELECTORS;
+	Viewmore.prototype.setup_button_event_handlers = function(){
+		var t = this;
 		// When button is clicked.
-		t.button.on('click', function(){
+		this.button.on('click', function(){
+			// Get posts
 			t.get_next_page_of_posts();
 		});
-
-		// Before send
-		_c_s.windo.on('nextpage--beforesend', function(){
-			console.log("Wait!!");
-			t.button_fill.css('will-change', 'transform');
-			TweenMax.to(t.button_fill, 5, {x: "15%", ease: Power3.easeOut});
-		});
-		// On progress
-		_c_s.windo.on('nextpage--progress', function(){
-			console.log("Progress!!");
-			var fill = (t.current_ready_state * 25) - 100;
-				fill += "%";
-			TweenMax.to(t.button_fill, 1, {x: fill, ease: Power3.easeOut});
-		});
-		// On next page success
-		_c_s.windo.on('nextpage--success', function(){
-			console.log("Success!!");
-		});
-		// On next page fail
-		_c_s.windo.on('nextpage--failed', function(){
-			console.log("Failed!!");
-		});
-		// On next page complete
-		_c_s.windo.on('nextpage--complete', function(){
-			console.log("Complete!!");
-			var tl = new TimelineMax();
-
-			tl.to(t.button_fill, 1, {x: "0%", ease: Power3.easeOut})
-				.to(t.button_fill, 1, {opacity: 0, ease: Power3.easeOut})
-				.to(t.button_fill, 0, {x: "-100%", opacity: 1});
-			t.button_fill.css('will-change', '');
-		});
 	}
+
 
 	Viewmore.prototype.get_next_page_of_posts = function(){
 		var t = this,
 			_c_s = _c.SELECTORS;
+
 
 		$.ajax({
 			url: '/janus/wp-json/posts',
 			dataType: 'json',
 			data: {category_name: 'insights'},
 			beforeSend: function( jqXHR ) {
+				// Temprarily disable button
+				t.button.off('click');
+
 				t.current_ready_state = jqXHR.readyState;
-				_c_s.windo.trigger('nextpage--beforesend');
+				console.log("Wait!!");
+				t.button_fill.css('will-change', 'transform');
+				TweenMax.to(t.button_fill, 5, {x: "15%", ease: Power3.easeOut});
 			},
 			xhrFields: {
 				onprogress: function (XHRProgressEvnt) {
 					t.current_ready_state = XHRProgressEvnt.srcElement.readyState;
-					_c_s.windo.trigger('nextpage--progress');
+					
+					console.log("Progress!!");
+					var fill = (t.current_ready_state * 25) - 100;
+						fill += "%";
+					TweenMax.to(t.button_fill, 1, {x: fill, ease: Power3.easeOut});
 				},
 			},
 		})
 		.done(function(result) {
 			console.log(result);
 			t.incomming_posts = result;
-			_c_s.windo.trigger('nextpage--success');
+			console.log("Success!!");
 		})
 		.fail(function() {
-			_c_s.windo.trigger('nextpage--failed');
+			console.log("Failed!!");
 		})
 		.always(function() {
-			_c_s.windo.trigger('nextpage--complete');
+			console.log("Complete!!");
+			var tl = new TimelineMax();
+			tl.to(t.button_fill, 0.8, {x: "0%", ease: Power3.easeOut}, "move_to_end")
+				.to(t.button_fill, 0.8, {opacity: 0, ease: Power3.easeOut}, "move_to_end+=0.2")
+				.to(t.button_fill, 0, {x: "-100%", opacity: 1, 
+					onComplete:function(){
+						t.setup_button_event_handlers();
+					}
+				});
+			t.button_fill.css('will-change', '');
 		});
 	}
 
