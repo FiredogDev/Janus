f9deb12ca703fe37bf1d27ba7cd4c6ea27b8c796
ammar88 <ammar.idris@aimail.info>
2015-04-08 11:07:20 +0100

    setup ajax call for new posts, setup XHR event listeners

--- library/js/app/viewmore.js
+++ library/js/app/viewmore.js
@@ -18,41 +18,73 @@ define([
 			t.button = button;
 
 			t.setup_event_listeners();
-
-			$.ajax({
-				url: '/janus/wp-json/posts',
-				dataType: 'json',
-				// data: {category_name: 'insights'},
-			})
-			.done(function(res) {
-				console.log(res);
-				console.log("success");
-			})
-			.fail(function() {
-				console.log("error");
-			})
-			.always(function() {
-				console.log("complete");
-			});
 	}
 
+	Viewmore.prototype.button = null;
+	Viewmore.prototype.incomming_posts = null;
 
-
-
-	Hoverboard.prototype.button = null;
-
-	Hoverboard.prototype.setup_event_listeners = function(){
+	Viewmore.prototype.setup_event_listeners = function(){
+		var t = this,
+			_c_s = _c.SELECTORS;
+		// When button is clicked.
 		t.button.on('click', function(){
-			var incomming_posts = get_next_page_of_posts();
+			t.get_next_page_of_posts();
+		});
+
+		// Before send
+		_c_s.windo.on('nextpage--beforesend', function(){
+			console.log("Wait!!");
+		});
+		// On progress
+		_c_s.windo.on('nextpage--progress', function(){
+			console.log("Progress!!");
+		});
+		// On next page success
+		_c_s.windo.on('nextpage--success', function(){
+			console.log("Success!!");
+		});
+		// On next page fail
+		_c_s.windo.on('nextpage--failed', function(){
+			console.log("Failed!!");
+		});
+		// On next page complete
+		_c_s.windo.on('nextpage--complete', function(){
+			console.log("Complete!!");
 		});
 	}
 
-	Hoverboard.prototype.get_next_page_of_posts = function(){
-		return "";
+	Viewmore.prototype.get_next_page_of_posts = function(){
+		var t = this,
+			_c_s = _c.SELECTORS;
+
+		$.ajax({
+			url: '/janus/wp-json/posts',
+			dataType: 'json',
+			data: {category_name: 'insights'},
+			beforeSend: function( jqXHR ) {
+				t.current_ready_state = jqXHR.readyState;
+				_c_s.windo.trigger('nextpage--beforesend');
+			},
+			xhrFields: {
+				onprogress: function (XHRProgressEvnt) {
+					t.current_ready_state = XHRProgressEvnt.srcElement.readyState;
+					_c_s.windo.trigger('nextpage--progress');
+				},
+			},
+			
+		})
+		.done(function(result) {
+			console.log(result);
+			t.incomming_posts = result;
+			_c_s.windo.trigger('nextpage--success');
+		})
+		.fail(function() {
+			_c_s.windo.trigger('nextpage--failed');
+		})
+		.always(function() {
+			_c_s.windo.trigger('nextpage--complete');
+		});
 	}
-
-
-
 
 	return Viewmore;
 
