b10d8c3e3dfcf8f680b776561f50342529390b11
ammar88 <ammar.idris@aimail.info>
2015-05-13 10:42:58 +0100

    inital findus content and street view placeholder

--- page-find-us.php
+++ page-find-us.php
@@ -1,29 +1,40 @@
 <?php get_header(); ?>
 	
 	<?php if (have_posts()) : ?>
-		
+
 	<main id="main" class="main cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 		<?php while (have_posts()) : the_post(); ?>
 
-		<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+		<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf prel' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+			
+			<div class="white-box wrap">
 
-			<header class="article-header hero_unit wrap cf">
-				<div class="hero_unit__wrapper width--full cf">
-					<div class="hero_unit__header cf">
-						<h1 class="hero_unit__title text-center width--full" itemprop="headline">
-							<!-- Main title -->
-							<span class="hero_unit__title--main"><?php the_title(); ?></span>
-						</h1>
+				<header class="article-header hero_unit cf">
+					<div class="hero_unit__wrapper width--full cf">
+						<div class="hero_unit__header cf">
+							<h1 class="hero_unit__title width--full" itemprop="headline">
+								<!-- Main title -->
+								<span class="hero_unit__title--main fl"><?php the_title(); ?></span>
+							</h1>
+						</div>
 					</div>
-				</div>
-			</header><?php // end article header ?>
+				</header><?php // end article header ?>
 
-			<section class="entry-content cf" itemprop="articleBody">
-				<?php
-				// the content
-				the_content(); ?>
-			</section><?php // end article section ?>
+				<section class="entry-content cf" itemprop="articleBody">
+
+						<?php
+						// the content
+						the_content(); ?>
+					
+				</section><?php // end article section ?>
+
+			</div>
+			
+			<!-- Street View -->
+			<div class="street-view">
+				
+			</div>
 
 		</article>
 
